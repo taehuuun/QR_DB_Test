cab8e902ddef8ba71556a70cec73a6069c175313
Hades000 <lsw463@naver.com>
2022-09-06 10:52:30 +0900

    Feat: DataManager클래스 싱글턴 패턴 적용

--- Assets/00.Scripts/DataManager.cs
+++ Assets/00.Scripts/DataManager.cs
@@ -7,4 +7,33 @@ using Firebase.Extensions;
 
 public class DataManager : MonoBehaviour
 {
+    private static DataManager ins;
+
+    public static DataManager Ins
+    {
+        get
+        {
+            if(ins == null)
+            {
+                return null;
+            }
+
+            return ins;
+        }
+    }
+
+    private void Awake()
+    {
+        #region ΩÃ±€≈Ê 
+        if(ins == null)
+        {
+            ins = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+        #endregion
+    
+    }
 }
