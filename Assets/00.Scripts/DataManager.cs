6ad9d71b0e8dde9a41cac1fb7fdc93ee457f0c04
Hades000 <lsw463@naver.com>
2022-09-06 10:20:32 +0900

    Add: Firebase 네임스페이스 추가

--- Assets/00.Scripts/DataManager.cs
+++ Assets/00.Scripts/DataManager.cs
@@ -2,17 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using Firebase.Firestore;
+using Firebase.Extensions;
+
 public class DataManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
