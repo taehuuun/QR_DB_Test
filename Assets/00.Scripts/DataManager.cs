5e189e50020dc8b99668939908a1766b1d4d85d0
Hades000 <lsw463@naver.com>
2022-09-06 15:01:04 +0900

    Revert "Add: Data 스크립트 추가"
    This reverts commit 6281751a312d157591afcb1426b36bd0b4804f68.

--- Assets/00.Scripts/DataManager.cs
+++ Assets/00.Scripts/DataManager.cs
@@ -11,8 +11,6 @@ public class DataManager : MonoBehaviour
 
     private string testQRData = "AAsad123sadDa123";
 
-    private FirebaseFirestore db = FirebaseFirestore.DefaultInstance;
-
     public static DataManager Ins
     {
         get
@@ -39,21 +37,5 @@ public class DataManager : MonoBehaviour
         }
         #endregion
     
-    }
-
-    /// <summary>
-    /// QR데이터가 DB내 데이터에 있는지 확인하는 함수
-    /// </summary>
-    /// <param name="QRdata">QR스캔 데이터</param>
-    private void ValidationData(string QRdata)
-    {
-        // QR데이터가 null이거나 길이가 2보다 작은 경우 리턴
-        if(QRdata == null || QRdata.Length < 2)
-            return;
-
-        string modelType = QRdata.Substring(0,2);
-        string shaVal = QRdata.Substring(2,QRdata.Length);
-
-        DocumentReference docRef = db.Collection(modelType).Document(shaVal);
     }
 }
