a142c5c36f091d4752587dd4de9c95ffc2a7d36c
Hades000 <lsw463@naver.com>
2022-09-06 16:13:32 +0900

    Feat: Data 클래스 파이어 베이스 데이터 추가

--- Assets/00.Scripts/Data/Data.cs
+++ Assets/00.Scripts/Data/Data.cs
@@ -1,18 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Firebase.Firestore;
 
-public class Data : MonoBehaviour
+[System.Serializable]
+[FirestoreData]
+public class Data
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [FirestoreProperty] public int effectVal {get; set;}
+    [FirestoreProperty] public int modelProdOrder {get; set;}
+    [FirestoreProperty] public string birth {get; set;}
+    [FirestoreProperty] public string clean {get; set;}
+    [FirestoreProperty] public string health {get; set;}
+    [FirestoreProperty] public string modelID {get; set;}
+    [FirestoreProperty] public string nickName {get; set;}
+    [FirestoreProperty] public string size {get; set;}
 }
